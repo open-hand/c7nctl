0ff5705146319b330c8b3c15200b4f6defc49156
vista yih <daqiang.yi@hand-china.com>
2020-03-16 22:20:27 +0800

    [FIX] remove vendor and refactor install.go

--- cmd/c7nctl/install.go
+++ cmd/c7nctl/install.go
@@ -18,6 +18,7 @@ import (
 	"github.com/choerodon/c7nctl/cmd/c7nctl/app"
 	"github.com/choerodon/c7nctl/pkg/utils"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/vinkdong/gox/log"
 	"io"
 )
@@ -28,80 +29,72 @@ var (
 )
 
 // installCmd represents the install command
-
 func newInstallCmd(out io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "install",
 		Short: "Install Choerodon",
 		Long:  `Install Choerodon quickly.`,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			if debug, _ := cmd.Flags().GetBool("debug"); debug {
-				log.EnableDebug()
-			}
-			skip, _ := cmd.Flags().GetBool("skip-input")
-
-			var (
-				mail string
-				err  error
-			)
-
-			c, err := utils.GetConfig()
-			if err != nil {
-				return err
-			}
-			if c.Terms.Accepted {
-				mail = c.OpsMail
-				goto start
-			}
-
-			if !skip {
-				utils.AskAgreeTerms()
-				mail, err = utils.AcceptUserInput(utils.Input{
-					Password: false,
-					Tip:      "请输入您的邮箱以便通知您重要的更新(Please enter your email address):  ",
-					Regex:    "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
-				})
-				if err != nil {
-					return err
-				}
-				c.Terms.Accepted = true
-				c.OpsMail = mail
-				c.Write()
-			} else {
-				log.Info("your are execute job by skip input option, so we think you had allowed we collect your information")
-			}
-
-		start:
-			err = app.Install(cmd, args, mail)
-			if err != nil {
-				log.Error("install failed")
-				return err
-			}
-			log.Success("Install succeed")
-			return nil
-		},
+		RunE:  runInstall,
 	}
 
-	cmd.Flags().StringVarP(&ResourceFile, "resource-file", "r", "", "Resource file to read from, It provide which app should be installed")
-	cmd.Flags().StringVarP(&ConfigFile, "config-file", "c", "", "User Config file to read from, User define config by this file")
-	cmd.Flags().String("version", "", "specify a version")
-	cmd.Flags().Bool("debug", false, "enable debug output")
-	cmd.Flags().Bool("no-timeout", false, "disable install job timeout")
-	cmd.Flags().String("prefix", "", "add prefix to all helm release")
-	cmd.Flags().Bool("skip-input", false, "use default username and password to avoid user input")
+	addFlags(cmd.Flags())
 
 	return cmd
 }
 
-func init() {
+func runInstall(cmd *cobra.Command, args []string) error {
+	if debug, _ := cmd.Flags().GetBool("debug"); debug {
+		log.EnableDebug()
+	}
+	skip, _ := cmd.Flags().GetBool("skip-input")
 
-	// Here you will define your flags and configuration settings.
+	var (
+		mail string
+		err  error
+	)
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// installCmd.PersistentFlags().String("foo", "", "A help for foo")
+	c, err := utils.GetConfig()
+	if err != nil {
+		return err
+	}
+	if c.Terms.Accepted {
+		mail = c.OpsMail
+		goto start
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// installCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	if !skip {
+		utils.AskAgreeTerms()
+		mail, err = utils.AcceptUserInput(utils.Input{
+			Password: false,
+			Tip:      "请输入您的邮箱以便通知您重要的更新(Please enter your email address):  ",
+			Regex:    "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
+		})
+		if err != nil {
+			return err
+		}
+		c.Terms.Accepted = true
+		c.OpsMail = mail
+		c.Write()
+	} else {
+		log.Info("your are execute job by skip input option, so we think you had allowed we collect your information")
+	}
+
+start:
+	err = app.Install(cmd, args, mail)
+	if err != nil {
+		log.Error("install failed")
+		return err
+	}
+	log.Success("Install succeed")
+	return nil
+}
+
+func addFlags(fs *pflag.FlagSet) {
+	fs.StringVarP(&ResourceFile, "resource-file", "r", "", "Resource file to read from, It provide which app should be installed")
+	fs.StringVarP(&ConfigFile, "config-file", "c", "", "User Config file to read from, User define config by this file")
+	fs.String("version", "", "specify a version")
+	fs.Bool("debug", false, "enable debug output")
+	fs.Bool("no-timeout", false, "disable install job timeout")
+	fs.String("prefix", "", "add prefix to all helm release")
+	fs.Bool("skip-input", false, "use default username and password to avoid user input")
 }
